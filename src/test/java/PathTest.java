bf0804dba63458c9f28ffda70ffcfaccd9cf5edc
anna-zagrebelnaya <annzagrebelnaya@gmail.com>
2020-01-03 17:10:21 +0200

    add maven and tests

--- src/test/java/PathTest.java
+++ src/test/java/PathTest.java
@@ -1,5 +1,38 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
 
-class PathTest {
+import static org.junit.Assert.assertEquals;
 
+public class PathTest {
+
+    @Test
+    public void cd_root() {
+        Path path = new Path("/a/b/c/d");
+        path.cd("/");
+
+        assertEquals("/", path.getPath());
+    }
+
+    @Test
+    public void cd_relational() {
+        Path path = new Path("/a/b/c/d");
+        path.cd("../../x");
+
+        assertEquals("/a/b/x", path.getPath());
+    }
+
+    @Test
+    public void cd_absolute() {
+        Path path = new Path("/a/b/c/d");
+        path.cd("/a/b/c");
+
+        assertEquals("/a/b/c", path.getPath());
+    }
+
+    @Test
+    public void cd_absoluteAndRelational() {
+        Path path = new Path("/a/b/c/d");
+        path.cd("/a/b/../x");
+
+        assertEquals("/a/x", path.getPath());
+    }
 }
